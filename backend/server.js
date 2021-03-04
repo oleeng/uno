296b42cc19beceb480ec06f402edfe3bb8cd7341
Ole Engelbrecht <ole_e@web.de>
2021-03-04 01:30:30 +0100

    backend as docker container

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,27 @@ const app = express();
 const http = require("http").createServer(app);
 //const bodyParser = require('body-parser');
 const io = require("socket.io")(http);
+const path = require('path');
+
+const redis = require("redis");
+const client = redis.createClient({
+    host: 'host.docker.internal',
+    port: 6379
+})
+
+client.on("error", function(error) {
+    console.error(error);
+});
 
 io.on('connection', (socket) => {
     socket.on('we lit', (msg) => {
+        client.get(msg, function(err, reply) {
+            // reply is null when the key is missing
+            console.log(reply)
+        });
         io.emit('ttt', "we lit as fuck on id: "+msg);
     });
     console.log('a user connected');
-    setTimeout((io) => {
-        io.emit('otto', "test")
-    }, 5000, io);
     socket.on('disconnect', () => {
         console.log('user disconnected')
     });
@@ -22,47 +34,14 @@ const port = process.env.PORT || 5000;
 //app.use(bodyParser.json());
 //app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(express.static('html'))
+
+app.get('/', function(req, res){
+    res.sendFile(path.resolve('html/test.html'));
+});
+
 app.get('/api/hello', (req, res) => {
-    let html = `
-    <!DOCTYPE html>
-        <html>
-          <head>
-            <meta charset="UTF-8">
-            <title>title</title>
-            <script src="/socket.io/socket.io.js"></script>
-            <script>
-              var socket = io()
-              let keys = location.search.substr(1).split("&")
-              
-              let id = ""
-              
-              for(let item of keys){
-                  console.log(item)
-                  let tmp = item.split("=");
-                  if(tmp[0] == "key"){
-                      id = tmp[1]
-                  }
-              }
-              
-              socket.emit('we lit', id);
-              
-              console.log(window.location.href)
-              
-              socket.on('otto', (msg) => {
-                console.log(msg)
-              });
-              
-              socket.on('ttt', (msg) => {
-                console.log(msg)
-              });
-            </script>
-          </head>
-          <body>
-          
-          </body>
-        </html>
-    `
-    res.send(html);
+    res.send("hello from express");
 });
 
 app.post('/api/world', (req, res) => {
